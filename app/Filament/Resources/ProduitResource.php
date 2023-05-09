1d3b1fa0856b4e4f5a8c289f59e2afecfb821881
Betsy-tr <betsytramma@gmail.com>
2023-05-09 09:33:26 -0400

    myshop

---
+++ app/Filament/Resources/ProduitResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProduitResource\Pages;
+use App\Filament\Resources\ProduitResource\RelationManagers;
+use App\Models\Categorie;
+use App\Models\Produit;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProduitResource extends Resource
+{
+    protected static ?string $model = Produit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('name')->required()->label('Nom'),
+                Select::make('categorie_id')->label('Categorie')
+                    /*->options(Categorie::all()->pluck('name','id'))
+                    ->searchable()*/
+                    ->relationship('categorie','name'),
+                Select::make('user_id')->label('Utilisateur')
+                ->relationship('user','name'),
+                RichEditor::make('description'),
+                TextInput::make('prix')->numeric(),
+                FileUpload::make('image')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                ImageColumn::make('image'),
+                TextColumn::make('name')->sortable(),
+                TextColumn::make('description')->limit(50),
+                TextColumn::make('prix')->money('eur')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProduits::route('/'),
+            'create' => Pages\CreateProduit::route('/create'),
+            'edit' => Pages\EditProduit::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Models/Produit.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Categorie;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Produit extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['categorie_id', 'user_id','name', 'description', 'prix', 'image'];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class) ; 
+    }
+
+    public function categorie() : BelongsTo
+    {
+        return $this->belongsTo(Categorie::class) ; 
+    }
+}
